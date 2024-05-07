d3a5b0c17e3ff618b42a7f536b168c69efa4c8e2
YABurlak <burlakov.iua@phystech.edu>
2024-05-07 20:16:21 +0300

    P30 working version

--- input_files/.ipynb_checkpoints/params-checkpoint.py
+++ input_files/.ipynb_checkpoints/params-checkpoint.py
@@ -1,10 +1,10 @@
 #Аэродинамические параметры		
-CL_take_off = 1.3
+CL_take_off = 1.7
 eta_prop = 0.7
 #Геометрические ограничения		
-A_aft = 0.8
-B_keel = 0.05
-l_stab = 0.6
+A_aft = 0.5
+B_keel = 0.03
+l_stab = 0.7
 #компоненты FPV-системы		
 mVTX = 0.024
 nVTX = 1
@@ -14,15 +14,21 @@ mRunc_s4 = 0.01
 nRunc_s4 = 1
 #Силовая установка		
 mReg = 0.025
-nReg = 2
+nReg = 1
 mAT2308 = 0.058
-nAT2308 = 1
+nAT2308 = 0
+mAT2312 = 0.13
+nAT2312 = 0
+mAT2814 = 0.132
+nAT2814 = 1
 m8X6_folding = 0.022
 n8X6_folding = 0
 m8X6_fix = 0.011
 n8X6_fix = 1
 mLiIon18650 = 0.047
-nLiIon18650 = 3
+nLiIon18650 = 0
+mKIT = 0.5
+nKIT = 1
 capacity_LiIon18650 = 0
 mLiIon21700 = 0.0068
 nLiIon21700 = 0
@@ -33,23 +39,42 @@ tube5X4 = 0.011
 tube8X7 = 0.016
 tube6X5 = 0.013
 tube8X6 = 0.032
-ltube10X9 = 0
+ltube10X9 = 0.8 * l_stab
 ltube5X4 = 0
-ltube8X7 = 0
+ltube8X7 = 0.0
 ltube6X5 = 0
-ltube8X6 = 0
+ltube8X6 = 4
 specific_wing = 1.05
 specific_aft = 1.05
-mfus = 0.15
+specific_glass = 0.07
+
+specific_petg = 1200
+specific_lwpla = 600
+spar1_pos = 0.2
+spar2_pos = 0.5
+spar3_pos = 0.8
+
+MGC = 0.2
+spar1_hight = 0.11 * MGC
+spar2_hight = 0.08 * MGC
+spar3_hight = 0.12 * MGC
+spar4_hight = spar3_hight
+
+spar_thickness = 0.8e-3
+
+mfus = 0.33
 nfus = 1
 #Cервы+провода		
 mMG90 = 0.015
-nMG90 = 0
+nMG90 = 7
 mES9051	= 0.006
-nES9051	= 4
+nES9051	= 0
 specific_3wire = 0.0095
 #Мозги+датчики		
 mPixhawk = 0.048
 nPixhawk = 1	
 mPito = 0.05
-nPito = 0
+nPito = 1
+#Полезная нагрузка
+mCargo = 0.8
+nCargo = 1
